2cac3b76277f9093eca69f43c740e7cab0f013ec
Katelyn <katelynjewel@gmail.com>
2021-11-23 10:16:53 -0800

    commit tree 28870a27e07ed96dd2e27ceae39986c35ee54d48

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors, setErrors] = useState([]);
+
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -125,6 +127,13 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
+        {errors.length > 0 && (
+          <ul style={{ color: "red" }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
